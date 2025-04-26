136394429877c58415d20f4612f7d1476b8cf8d3
Danil Shifman <danilshifman07@gmail.com>
2025-04-26 18:50:24 +0300

    - fix pep8

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from api.views import ShortLinkRedirectView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
