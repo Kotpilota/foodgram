e0a3b8c20d7b66a261ce5053d088106a15fa2d5a
Danil Shifman <danilshifman07@gmail.com>
2025-04-28 11:02:34 +0300

    - add migrations

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,9 +8,9 @@ app_name = 'api'
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
 
-router.register('tags', TagViewSet)
-router.register('ingredients', IngredientViewSet)
-router.register('recipes', RecipeViewSet)
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
